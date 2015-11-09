8e23a61b8a7e6151cd464afacf51c2989e2f0ff9
Peter Bakkum <pbb7c@virginia.edu>
2015-11-08 16:51:49 -0800

    Thick width lines

--- ico.go
+++ ico.go
@@ -6,9 +6,11 @@
 
 package main
 
-import _ "fmt"
+//import "fmt"
 import "math"
 import "time"
+import "log"
+import "os"
 //import "strconv"
 import "github.com/nsf/termbox-go"
 
@@ -17,6 +19,7 @@ var EDGE rune = '·'
 var BLANK rune = ' '
 var CANVASXSCALE float64 = 2.2
 var CANVASYSCALE float64 = 1.0
+var logger *log.Logger
 
 func OrthographicProjection(point *D3Point, offsetX, offsetZ float64) *D2Point {
 	aVal := [][]float64{
@@ -76,6 +79,83 @@ func ProjectPointsOntoCanvas(points []*D3Point, canvas *Canvas, offsetRow, offse
 	}
 }
 
+//FILLS := map[int]int[]{
+//	0:	 ['|', '|', '|'],
+//	45:	 ['/', '/', '/'],
+//	90:  ['_', '-', '-'],
+//	135: [',', '\\', '`'],
+//]
+
+func pointLineDist(x0, y0, deltaX, deltaY, adj, length float64) float64 {
+	return math.Abs(deltaY * x0 - deltaX * y0 + adj) / length
+}
+
+func pointDist(x0, y0, x1, y1 float64) float64 {
+	return math.Sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0))
+}
+
+func WideLineProjection(canvas *Canvas, x2, x1, y2, y1, width float64) {
+	if x2 < x1 {
+		tmpX := x2
+		tmpY := y2
+		x2 = x1
+		y2 = y1
+		x1 = tmpX
+		y1 = tmpY
+	}
+
+	maxX := int(canvas.Width())
+	maxY := int(canvas.Height())
+	deltaX := x2 - x1
+	deltaY := y2 - y1
+
+	var m float64
+	if math.Abs(x2 - x1) < 0.000001 {
+		m = math.NaN()
+	} else {
+		m = (y2 - y1) / (x2 - x1)
+	}
+	b := y1 - m * x1
+
+	adj := x2 * y1 - y2 * x1
+	length := math.Sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1))
+
+	var y0 float64
+
+	for x0 := round(x1 - width - 1); x0 <= round(x2 + width + 1) && x0 <= maxX; x0++ {
+		//logger.Printf("(%f,%f) (%f,%f) : %d, %f\n", x1, y1, x2, y2, x0, y0)
+		if !math.IsNaN(m) {
+			y0 = m * float64(x0) + b
+		} else {
+			y0 = y1
+		}
+
+		for yi := round(y0); yi >= 0 && yi <= maxY; yi++ {  // exit condition
+			lineDist := pointLineDist(float64(x0), float64(yi), deltaX, deltaY, adj, length)
+			pointDist1 := pointDist(float64(x0), float64(yi), x1, y1)
+			pointDist2 := pointDist(float64(x0), float64(yi), x2, y2)
+
+			if lineDist > width || pointDist1 + pointDist2 > length + 2 {
+				break
+			}
+
+			canvas.Set(yi, x0, EDGE)
+		}
+
+		for yi := round(y0) - 1; yi >= 0 && yi <= maxY; yi-- {  // exit condition
+			lineDist := pointLineDist(float64(x0), float64(yi), deltaX, deltaY, adj, length)
+			pointDist1 := pointDist(float64(x0), float64(yi), x1, y1)
+			pointDist2 := pointDist(float64(x0), float64(yi), x2, y2)
+
+			if lineDist > width || (pointDist1 + pointDist2) > length + 2 {
+				break
+			}
+
+			canvas.Set(yi, x0, EDGE)
+		}
+	}
+}
+
 func BresenhamProjection(canvas *Canvas, x0, x1, y0, y1 int) {
 	if x1 < x0 {
 		tmpX := x0
@@ -124,11 +204,11 @@ func ProjectEdgesOntoCanvas(edges []*Edge, canvas *Canvas, offsetRow, offsetColu
     }
 		p0 := OrthographicProjection(edge.P0, offsetX, offsetZ)
 		p1 := OrthographicProjection(edge.P1, offsetX, offsetZ)
-		x0 := round(p0.X()*CANVASXSCALE) + int(offsetColumn)
-		x1 := round(p1.X()*CANVASXSCALE) + int(offsetColumn)
-		y0 := round(p0.Y()*CANVASYSCALE) + int(offsetRow)
-		y1 := round(p1.Y()*CANVASYSCALE) + int(offsetRow)
-		BresenhamProjection(canvas, x0, x1, y0, y1)
+		x0 := p0.X()*CANVASXSCALE + float64(offsetColumn)
+		x1 := p1.X()*CANVASXSCALE + float64(offsetColumn)
+		y0 := p0.Y()*CANVASYSCALE + float64(offsetRow)
+		y1 := p1.Y()*CANVASYSCALE + float64(offsetRow)
+		WideLineProjection(canvas, x0, x1, y0, y1, 1.5)
 	}
 }
 
@@ -171,12 +251,20 @@ func printCanvasToTermbox(canvas *Canvas) {
 }
 
 func main() {
+	f, err := os.OpenFile("ico.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	logger = log.New(f, "ico: ", log.Lshortfile)
+	logger.Print("Starting ico run")
+
 	event_queue := initTermbox()
 
 	width := uint(120)
 	height := uint(60)
 	canvas := NewCanvas(height, width)
-	model := MakeIcosahedron(2)
+	model := MakeIcosahedron(1)
 	model.Scale(30)
 	xOffset := int(width) / 2
 	yOffset := int(float64(height) / 1.5)
